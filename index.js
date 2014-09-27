390eb9cbe914a32a6d1119b264a0d4f0d7cdc46c
Ajay Gandhi <aag255@cornell.edu>
2014-09-26 20:53:00 -0400

    Document all the things

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 
+// Config object
 var config = {
 	appID: '698243363597060', 
 	secret: 'ed984e63cb8d378122fc5bd43dc962d6',
 	token: 'CAAJ7DHOnawQBAA6AFS68RPaasmyRzbl9cyhiXHMLB6ZBMj4W4VdvrK06xa8BZCYOWDm1ZAE32hA6LfXn8SvPBjDOk9IGZAfIirLgpnArbvE70rgU9g2RM2RnjSdaqU57C9vEZApuueTPUUD5qDIkjo50hcjz9FZAnlEnnNFWDie1jZBZCQZBxRK6wcdmaZBispeZCZBh2zlZCfZAE1lQ6bw1Bhy70wD9jWIgPXjgsZD'
 }
 
+// Include the sdk
 var Facebook = require('facebook-node-sdk');
 
-// Connect to facebook
+// Func to connect to facebook
 createFB = function(appId,secret,token) {
   return new Facebook({
     appID: appId,
@@ -15,9 +17,10 @@ createFB = function(appId,secret,token) {
   }).setAccessToken(token);
 };
 
+// Connect to facebook
 var fb = createFB(config.appID, config.secret, config.token)
 
-
+// Do things!
 fb.api('/me', function(err, data) {
 	console.log(err);
   	console.log(data); // => { id: ... }
