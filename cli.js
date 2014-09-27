6f73a70ef62164a8099e427659e893644d0bb5b6
Ajay Gandhi <aag255@cornell.edu>
2014-09-26 22:48:21 -0400

    Create help command

--- cli.js
+++ cli.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
 // ^ Make it a cli
 
+// Require some modules
+var fs = require('fs');
+var chalk = require('chalk');
+
 var script_name = "./cli.js";
 
 // Args always includes "node" and the current script, so remove them
@@ -12,50 +16,36 @@ var commands = ["help", "newsfeed", "post", "poke"];
 // Parse arguments
 if (args.length >= 1) {
   var cmd = args[0];
-  if (commands.indexOf(elm) == -1) {
+  if (commands.indexOf(cmd) == -1) {
     console.log("Invalid argument. Run `" + script_name + " help` for help.");
-  } else if (cmd == "--help") {
-    // Display help page
+  } else if (cmd == "help") {
+    console.log();
+    fs.readFile("man.txt", "utf8", function(err, data) {
+      if (err) {
+        return console.log("Error reading manual.");
+      }
+      var manual = JSON.parse(data);
+      console.log(chalk.bold(manual.header));
+      console.log("\n");
+      var contents = manual.categories;
+      contents.forEach(function(elm) {
+        console.log(chalk.bold(elm.catName));
+        console.log("\t" + elm.content + "\n");
+      });
+    });
   } else if (cmd == "newsfeed") {
     // Display newsfeed
   } else if (cmd == "post") {
     if (args[1] != undefined) {
-      // Post message
+      var message = args[1];
     } else {
       console.log("Invalid argument. `" + script_name + " post` takes an argument.");
     }
   } else if (cmd == "poke") {
     if (args[1] != undefined) {
-      // Poke person
+      var pokee = args[1];
     } else {
       console.log("Invalid argument. `" + script_name + " post` takes an argument.");
     }
   }
 }
-
-// Config object
-var config = {
-  appID: '698243363597060', 
-  secret: 'ed984e63cb8d378122fc5bd43dc962d6',
-  token: 'CAAJ7DHOnawQBAA6AFS68RPaasmyRzbl9cyhiXHMLB6ZBMj4W4VdvrK06xa8BZCYOWDm1ZAE32hA6LfXn8SvPBjDOk9IGZAfIirLgpnArbvE70rgU9g2RM2RnjSdaqU57C9vEZApuueTPUUD5qDIkjo50hcjz9FZAnlEnnNFWDie1jZBZCQZBxRK6wcdmaZBispeZCZBh2zlZCfZAE1lQ6bw1Bhy70wD9jWIgPXjgsZD'
-}
-
-// Include the sdk
-var Facebook = require('facebook-node-sdk');
-
-// Func to connect to facebook
-createFB = function(appId,secret,token) {
-  return new Facebook({
-    appID: appId,
-    secret: secret
-  }).setAccessToken(token);
-};
-
-// Connect to facebook
-var fb = createFB(config.appID, config.secret, config.token)
-
-// Do things!
-fb.api('/me', function(err, data) {
-  console.log(err);
-    console.log(data); // => { id: ... }
-});
